8e37875cadd0ea275a9f92492c846bb06b4f1cd3
Luis Toro <l-toro@outlook.com>
2021-04-20 18:30:01 -0500

    Unicafe Step 2

--- unicafe/src/index.js
+++ unicafe/src/index.js
@@ -19,7 +19,7 @@ const App = (props) => {
       <p>bad {bad}</p>
       <p>all {(good + neutral + bad)}</p>
       <p>average {(good - bad)/(good + neutral + bad)}</p>
-      <p>positive {(good/(good + neutral + bad) * 100)}</p>
+      <p>positive {(good/(good + neutral + bad) * 100)}%</p>
     </div>
   )
 }
